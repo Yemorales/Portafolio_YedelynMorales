d6bdfcce423297b0a8533c649e260267bdc37a3c
Yedelyn <105745378+Yemorales@users.noreply.github.com>
2026-05-20 20:10:09 -0600

    Commit 1
    Creacion del proyecto

---
+++ src/test/java/TechShop/Yedelyn/TechShopApplicationTests.java
@@ -0,0 +1,13 @@
+package TechShop.Yedelyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
